72b9856817eeff79df2f2178e0b58aacaf98c6f9
moon4656 <moon4656@gmail.com>
2025-02-10 21:17:32 +0900

    version4

--- basic_syntax/basic_command.sh
+++ basic_syntax/basic_command.sh
@@ -1,2 +1,8 @@
 ## git add . 을 모든변경사항을 stage area 이동
 git add .
+
+# git commit은 staging area의 변경사항을 확정짓고 commitID생성
+git commit -m "메세지"
+
+# 원격 repo에 main브랜치 변경사항(commitID)을 업로드
+git push orgin main
